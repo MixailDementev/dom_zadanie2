1946812f1045a82babe09a3bce3ec23c7f47c4a2
MDementev <vanneste1207@gmail.com>
2022-08-27 10:08:45 +0300

    добавил задание3

--- zadanie_3/Program.cs
+++ zadanie_3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number <= 7)
+{
+    if(number >= 1 && number <= 5)
+    {
+        System.Console.WriteLine("Будни");
+        break;
+    }
+    else{
+        System.Console.WriteLine("Выходной");
+        break;
+    }
+}    
+    
